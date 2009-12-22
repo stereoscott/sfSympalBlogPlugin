dc9e8d8c6013ec3142606cbb47d4706d7e8fd48d
Jonathan H. Wage <jonwage@gmail.com>
2009-12-22 17:18:15 +0000

    [1.4][sfSympalBlogPlugin][0.7] Updating blog plugin for 0.7 after removing content templates

--- lib/sfSympalBlogPluginInstall.class.php
+++ lib/sfSympalBlogPluginInstall.class.php
@@ -6,28 +6,16 @@ class sfSympalBlogPluginInstall extends sfSympalPluginManagerInstall
   {
     $installVars['content']['sfSympalBlogPost']['teaser'] = 'This is the teaser line for the sample blog post';
     $installVars['content']->save();
+    $installVars['contentType']->setTemplate('sympal_blog/view');
     $installVars['contentType']->save();
 
     $installVars['menuItem']['label'] = 'Blog';
 
     $this->saveMenuItem($installVars['menuItem']);
 
-    $properties = array(
-      'path' => 'sympal_blog/view',
-      'is_default' => true
-    );
-    $contentTemplate = $this->newContentTemplate('View BlogPost', $installVars['contentType'], $properties);
-    $contentTemplate->save();
-
-    $properties = array(
-      'path' => 'sympal_blog/list'
-    );
-    $contentTemplate = $this->newContentTemplate('List BlogPost', $installVars['contentType'], $properties);
-    $contentTemplate->save();
-
+    $installVars['contentList']->setTemplate('sympal_blog/list');
     $installVars['contentList']->sort_column = 'id';
     $installVars['contentList']->sort_order = 'DESC';
-    $installVars['contentList']->Template = $contentTemplate;
     $installVars['contentList']->save();
   }
 }
