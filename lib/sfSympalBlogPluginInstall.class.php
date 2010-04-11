5be325a59068938dc0dd55e4c1512c42eefc6895
Ryan Weaver <ryan@thatsquality.com>
2010-04-11 14:07:01 -0500

    [1.0] Changing label when installing

--- lib/sfSympalBlogPluginInstall.class.php
+++ lib/sfSympalBlogPluginInstall.class.php
@@ -14,6 +14,7 @@ class sfSympalBlogPluginInstall extends sfSympalPluginManagerInstall
     $slot->save();
 
     $installVars['menuItem']['name'] = 'Blog';
+    $installVars['menuItem']['label'] = 'Blog';
 
     $this->saveMenuItem($installVars['menuItem']);
 
