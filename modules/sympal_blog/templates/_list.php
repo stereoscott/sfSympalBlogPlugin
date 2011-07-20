3e8dfd7dfb181761db91cd33b876c084e8f3132d
nippo <ippolito.nicolas@gmail.com>
2011-07-20 10:36:41 +0200

    Template

--- modules/sympal_blog/templates/_list.php
+++ modules/sympal_blog/templates/_list.php
@@ -1,17 +1,11 @@
 <?php echo auto_discovery_link_tag('rss', $sf_sympal_content->getRoute().'.rss') ?>
-
 <div id="two-columns">
-
     <div class="box bottom">
-
         <?php echo get_sympal_content_slot('body') ?>
         <?php $listResults = $pager->getResults() ?>
         <?php echo get_partial('sympal_blog/blog_list', array('menuItem' => $menuItem, 'pager' => $pager, 'contents' => $listResults)) ?>
-
     </div>
-
 </div>
-
 <?php slot('sidebar') ?>
     <?php echo get_component('sympal_blog', 'sidebar') ?>
 <?php end_slot() ?>
